1170610a43cb826486ed9bb2861d1b560e0d57ea
Gage B. <gagebarbour96@gmail.com>
2025-11-29 20:59:09 -0600

    Fixed Discord Login on PROD

--- config/services.php
+++ config/services.php
@@ -38,7 +38,7 @@ return [
     'discord' => [
         'client_id' => env('DISCORD_CLIENT_ID'),
         'client_secret' => env('DISCORD_CLIENT_SECRET'),
-        'redirect' => env('DISCORD_REDIRECT_URI'),
+        'redirect' => env('DISCORD_REDIRECT_URI', '/login/discord/handle'),
 
         // optional
         'allow_gif_avatars' => (bool) env('DISCORD_AVATAR_GIF', true),
